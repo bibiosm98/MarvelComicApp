5142949c37fef336ed9916475537bdfcc8842c70
Norbert <bibiosm98@gmail.com>
2021-04-10 19:33:40 +0200

    check

--- app/src/main/java/com/example/marvelcomicappbykotlin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/marvelcomicappbykotlin/ui/home/HomeFragment.kt
@@ -18,16 +18,7 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-//        homeViewModel =
-//                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-//        val root = inflater.inflate(R.layout.fragment_home, container, false)
-////        val textView: TextView = root.findViewById(R.id.text_home)
-////        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-////            textView.text = it
-////        })
-//        return root
 
-//        val binding = FragmentHomeBinding.inflate(inflater)
         val binding = FragmentHomeBinding.inflate(inflater)
 
         binding.lifecycleOwner = this
